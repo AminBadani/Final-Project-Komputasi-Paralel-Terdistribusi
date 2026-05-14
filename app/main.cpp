ca31c5eed624eb72783e2b296bc9a6a1f668eb87
AminBadani <ahmadaminbadani@gmail.com>
2026-05-14 20:44:57 +0700

    update main.cpp, percobaan paralel

--- app/main.cpp
+++ app/main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <cstdlib>
+#include <stdexcept>
+#include <omp.h>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+int main(int argc, char** argv) {
+    int count = 10;
+    int cores = 1;
+
+    try {
+        if (argc > 1) count = std::atoi(argv[1]);
+        if (argc > 2) cores = std::atoi(argv[2]) > 4 ? 4 : std::atoi(argv[2]);
+    } catch (const std::invalid_argument&) {
+        std::cerr << "Invalid integer argument\n";
+        return 0;
+    }
+
+    #pragma omp parallel for num_threads(cores)
+    for (int i = 0; i < count; i++) {
+        int thread_num = omp_get_thread_num();
+        std::cout << "From: " << thread_num << " - Hello world " << i << "\n";
+    }
+
+    return 1;
 }
