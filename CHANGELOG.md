c55bf8e43903b74b58f37d6c0e38a86c0cf43c54
AminBadani <ahmadaminbadani@gmail.com>
2026-06-02 13:58:49 +0700

    memperbaiki perhitungan durasi global dan menambahkan durasi lokal

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,21 +1,27 @@
-## Progress Log — [1 Juni 2026]
+## Progress Log — [2 Juni 2026]
 
 ### Dikerjakan Hari Ini
-- Perubahan struktur kode program;
-    - menghilangkan menu dan mengganti dengan input *argument* `total_numbers = argv[1]` dan `active_workers = argv[2]` sebelum *running*
-    - menambahkan `send-receive` hasil dari *workers* ke *master* 
-- Fix bug: menyelesaikan *bug* `std::cout` dari *workers* yang terlambat di tampilkan
-- Unit test: akurasi; skalabilitas;
+- Memperbaiki perhitungan durasi global (`duration_global`) dan menambahkan durasi lokal (`duration_local`)
+    - `duration_global = MPI_WTime()`
+    - `duration_local = omp_get_wtime()`
+- Menambahkan beberapa test *edge case*
+    - `start = 0` dan `end = 0`
+    - `start = -10` dan `end = 5`
+    - `start = 10` dan `end = 5`
+    - `active_cores <= 0`
+    - `active_cores > limit `
+- Fix bug: -
+- Unit test: akurasi; edge case; skalabilitas;
 
 ### Kendala / Blockers
 - Deskripsi: -
 - Solusi: -
 
 ### Rencana Besok
-- Implementasi testing *edge case*
-- Implementasi checkpoint: replikasi `local_sum`, *send* dari *workers* ke *master* setiap `n` detik
+- Implementasi *live progress* setiap `n` persen
+- Implementasi *checkpoint*: replikasi `local_sum`, *send* dari *workers* ke *master* setiap `n` detik
 
 ### Commit Hari Ini
-- feat: Perubahan struktur program dengan menghilangkan menu
-- fix: Memperbaiki `std::cout` hasil *workers* yang terlambat ditampilkan
-- test: akurasi (PASS); skalabilitas (PASS);
+- feat: 
+- fix: 
+- test: akurasi (PASS); edge case (PASS); skalabilitas (PASS);
--- app/main.cpp
+++ app/main.cpp
@@ -4,7 +4,6 @@
 #include <omp.h>
 #include <mpi.h>
 #include <string>
-#include <chrono>
 
 #include "parallel.h"
 
@@ -15,10 +14,9 @@ struct SystemConfig {
 
 int main(int argc, char** argv) {
     MPI_Init(&argc, &argv);
-    
-    int world_rank; 
-    int world_size;
-    int world_len;
+
+    int world_rank, world_size, world_len;
+    double start_global, end_global, duration_global;
     char current_node_name[MPI_MAX_PROCESSOR_NAME];
     SystemConfig current_config;
 
@@ -32,7 +30,10 @@ int main(int argc, char** argv) {
         MPI_Send(current_node_name, MPI_MAX_PROCESSOR_NAME, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
 
     } else {
-        printf("\n-- Containers count: %d\n", world_size);
+        // Start global time 
+        start_global = MPI_Wtime();
+
+        printf("\n=== Containers count: %d\n", world_size);
         printf("Master node (rank %d) is running on container: %s\n", world_rank, current_node_name);
 
         // Master collects names from all other ranks (from other workers)
@@ -45,14 +46,15 @@ int main(int argc, char** argv) {
         // Initialize config on master
         current_config = SystemConfig{
             .total_numbers=( argc > 1 ? std::stoll(argv[1]) : 1000000000 ), 
-            .active_cores=( argc > 2 ? ( std::stoi(argv[2]) > 8 ? 8 : std::stoi(argv[2]) ) : 4 )
+            .active_cores=( argc > 2 
+                ? ( std::stoi(argv[2]) > 8 
+                        ? 8 
+                        : std::stoi(argv[2]) ) 
+                : 4 )
         };
     }
 
-    // Start time 
-    auto start_time = std::chrono::high_resolution_clock::now();
-
-    // 2. Broadcast the config from Master (0) to ALL workers
+    // Broadcast the config from Master (0) to ALL workers
     MPI_Bcast(&current_config, sizeof(SystemConfig), MPI_BYTE, 0, MPI_COMM_WORLD);
 
     // Split the workload evenly to workers
@@ -60,11 +62,17 @@ int main(int argc, char** argv) {
     long long start = (world_rank * chunk) + 1;
     long long end = (world_rank == world_size - 1) ? current_config.total_numbers : start + chunk - 1;
 
-    // Process local sum on eachy workers
+    // Process local sum on each workers
+    double start_local  = omp_get_wtime();
     double local_sum = compute_parallel_sqrt_sum(start, end, current_config.active_cores);
+
+    double end_local  = omp_get_wtime();
+    double duration_local = end_local - start_local;
+
     if (world_rank != 0) {
+        double local_result[2] = {local_sum, duration_local};
         long long local_range[2] = {start, end};
-        MPI_Send(&local_sum, 1, MPI_DOUBLE, 0, 1, MPI_COMM_WORLD);
+        MPI_Send(local_result, 2, MPI_DOUBLE, 0, 1, MPI_COMM_WORLD);
         MPI_Send(local_range, 2, MPI_LONG_LONG, 0, 2, MPI_COMM_WORLD);
     }
 
@@ -72,32 +80,37 @@ int main(int argc, char** argv) {
     double global_sum = 0;
     MPI_Reduce(&local_sum, &global_sum, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
 
-    // End time
-    auto end_time = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> duration = end_time - start_time;
-
     if (world_rank == 0) {
         // Print from Master
-        std::cout << "\nContainer Rank " << 0 << " computed range [" 
-                << start << " to " << end
-                << "] using " << current_config.active_cores << " cores. Local Sum: " << std::fixed << local_sum << "\n";
+        std::cout << "\nContainer Rank " << 0
+                << " computed range [" << start << " to " << end << "]" 
+                << " using " << current_config.active_cores << " cores" 
+                << " \n   -- Local sum: " << std::fixed << local_sum  << "."
+                << " \n   -- Local duration: " << duration_local << " seconds.\n";
 
         // Send each workers result to Master
-        double worker_sum;
+        double worker_result[2];
         long long worker_range[2];
         for (int i = 1; i < world_size; i++) {
-            MPI_Recv(&worker_sum, 1, MPI_DOUBLE, i, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            MPI_Recv(worker_result, 2, MPI_DOUBLE, i, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             MPI_Recv(worker_range, 2, MPI_LONG_LONG, i, 2, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            long long worker_start = worker_range[0];
-            long long worker_end = worker_range[1];
 
-            std::cout << "Container Rank " << i << " computed range [" 
-                << worker_start << " to " << worker_end
-                << "] using " << current_config.active_cores << " cores. Local Sum: " << std::fixed << worker_sum << "\n";
+            auto [worker_sum, worker_duration] = worker_result;
+            auto [worker_start, worker_end] = worker_range;
+
+            std::cout << "\nContainer Rank " << i 
+                << " computed range [" << worker_start << " to " << worker_end << "]" 
+                << " using " << current_config.active_cores << " cores" 
+                << " \n   -- Local sum: " << std::fixed << worker_sum  << "."
+                << " \n   -- Local duration: " << worker_duration << " seconds.\n";
         }
 
-        std::cout << "\n--- TOTAL DISTRIBUTED SUM: " << std::fixed << global_sum << "\n";
-        std::cout << "--- TOTAL TIME:  " << duration.count() << " seconds.\n";
+        // End global time
+        end_global = MPI_Wtime();
+        duration_global = end_global - start_global;
+
+        std::cout << "\n=== TOTAL DISTRIBUTED SUM: " << std::fixed << global_sum << "\n";
+        std::cout << "=== TOTAL TIME: " << duration_global << " seconds.\n";
     }
 
     MPI_Finalize();
